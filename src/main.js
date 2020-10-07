48d5c623040591cf0f0214e43f99327aa5d19eab
EudesKeyboard <eudes.jf@gmail.com>
2020-10-07 14:07:57 -0300

    Adding Hello component, vuex, vue-router, stores

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
+import { sync } from 'vuex-router-sync'
+
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+
+Vue.use(Vuex)
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+import routes from './router'
+import VuexStore from './vuex/store'
+
+const store = new Vuex.Store(VuexStore);
+const router = new VueRouter({
+  routes
+})
+
+sync(store, router)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
+  store,
 }).$mount('#app')
